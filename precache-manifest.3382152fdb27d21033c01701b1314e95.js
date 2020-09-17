69e4c8211ee6c2a1cd44e70cf60a04a790d813cd
Warefoxe <30841026+Warefoxe@users.noreply.github.com>
2020-09-17 20:56:54 +0300

    Updates

---
+++ precache-manifest.3382152fdb27d21033c01701b1314e95.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ecb9d2d98464ad622912e7d3833328f",
+    "url": "/TransportReact/index.html"
+  },
+  {
+    "revision": "9588996c50071d8cdf1f",
+    "url": "/TransportReact/static/css/main.524d425e.chunk.css"
+  },
+  {
+    "revision": "0b10d53327c80b38a788",
+    "url": "/TransportReact/static/js/2.c8ec21e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TransportReact/static/js/2.c8ec21e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9588996c50071d8cdf1f",
+    "url": "/TransportReact/static/js/main.89e53937.chunk.js"
+  },
+  {
+    "revision": "9695a950fc169d93a8e5",
+    "url": "/TransportReact/static/js/runtime-main.5f4e4051.js"
+  },
+  {
+    "revision": "425975a640d49aa989a77f668f1841f7",
+    "url": "/TransportReact/static/media/bg2.425975a6.jpg"
+  },
+  {
+    "revision": "275bbb78ef61d9842e263b99b658d9e4",
+    "url": "/TransportReact/static/media/bg3.275bbb78.jpg"
+  }
+]);
